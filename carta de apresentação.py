010d2dded9daab1e55ead505b8b910e17d04b606
odavimart <162045396+odavimart@users.noreply.github.com>
2024-05-08 19:08:57 -0300

    Carta de Apresentação
    Imagine que você quer que alguém veja seu perfil no LinkedIn. Em vez de apenas enviar o link do seu perfil, você pode criar um script. Este script faz com que, quando alguém o execute, abra automaticamente o navegador, vá até o LinkedIn e carregue o seu perfil. Assim, a pessoa interessada será redirecionada diretamente para o seu perfil no LinkedIn, onde poderá ver todas as suas informações profissionais. É como se você estivesse guiando-os até lá, de forma automática.

---
+++ carta de apresentação.py
@@ -0,0 +1,42 @@
+''' 
+
+Davi Martins
+Salvador, Bahia, Brasil
+davi.martsousa@gmail.com
+(71) 9 9717-4385
+
+Prezados,
+
+Sou Davi, um profissional de 24 anos, natural de Salvador, Bahia, com grande entusiasmo e interesse em oportunidades profissionais na 
+T.I (Tecnologia da Informação). Possuo uma sólida formação acadêmica e experiência prática que me capacitam a contribuir de forma significativa
+ em projetos desafiadores e dinâmicos.
+
+Ao longo dos meus estudos e experiências profissionais, desenvolvi habilidades em [mencionar suas principais competências,
+como liderança, comunicação, resolução de problemas, etc.]. Além disso, tenho um forte compromisso com a excelência e estou
+sempre em busca de novos desafios que me permitam crescer e aprender.
+
+Estou confiante de que minha dedicação, habilidades e determinação me tornam um candidato ideal para oportunidades em sua organização.
+Estou ansioso para discutir como posso contribuir para o sucesso da equipe e participar do crescimento da empresa.
+
+Agradeço antecipadamente pela consideração da minha candidatura e espero ter a oportunidade de discutir mais sobre minha experiência
+e como posso agregar valor à sua organização.
+
+Atenciosamente,
+
+Davi'''
+
+'''Imagine que você quer que alguém veja seu perfil no LinkedIn. Em vez de apenas enviar o link do seu perfil, você pode criar um script.
+Este script faz com que, quando alguém o execute, abra automaticamente o navegador, vá até o LinkedIn e carregue o seu perfil.
+Assim, a pessoa interessada será redirecionada diretamente para o seu perfil no LinkedIn, onde poderá ver todas as suas informações
+profissionais. É como se você estivesse guiando-os até lá, de forma automática.'''
+
+import pyautogui
+
+pyautogui.PAUSE = 0.3
+
+pyautogui.press('win')
+pyautogui.write('edge')
+pyautogui.press('enter')
+pyautogui.write('https://www.linkedin.com/in/davi-martins-b89b15178/')
+pyautogui.press('enter')
+
